d9a3ff4917f2ac17dc819e2b2eada8f7c93eb3ee
Proga 404 Dev <proga.404.dev@gmail.com>
2021-11-10 00:34:25 +0100

    ✌️ Added swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  @ApiOkResponse({ description: 'Return Hello World' })
   getHello(): string {
-    return this.appService.getHello();
+    try {
+      return this.appService.getHello();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,20 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as os from 'os';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({});
+
+  const config = new DocumentBuilder()
+    .setTitle('Proga Works API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {});
+  SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(port);
   Logger.log(`Server running on ${os.hostname}:${port}`, 'Bootstrap');
