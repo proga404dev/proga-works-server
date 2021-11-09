2b8923998a8f481d357e7957a0c38e3a53cc5a88
Proga 404 Dev <proga.404.dev@gmail.com>
2021-11-10 00:22:19 +0100

    💪 Added logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor,
+  },],
 })
 export class AppModule { }
