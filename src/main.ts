1daf131e8459ab66a213ba6a58499565ce1782b5
Proga 404 Dev <proga.404.dev@gmail.com>
2021-11-10 00:18:45 +0100

    👌 Added config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as os from 'os';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({});
+
+  await app.listen(port);
+  Logger.log(`Server running on ${os.hostname}:${port}`, 'Bootstrap');
 }
 bootstrap();
+
